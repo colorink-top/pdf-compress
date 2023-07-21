1f48c3a9a7a8411318917db7cb6edf9e2d77ceed
suziwen <suziwen1@gmail.com>
2023-07-21 09:11:27 +0800

    add wasm cache

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,27 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+    plugins: [react(), VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /gs\..+\.wasm$/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'wasm',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      }
+    })],
     build: {target: 'esnext'},
     base: "/pdf-compress"
 })
